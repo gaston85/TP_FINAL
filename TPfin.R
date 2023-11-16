c24af4d3a59614ced4cc752dc791c52845f816a7
Gaston_Luna <gaston.e.luna01@gmail.com>
2023-11-16 12:47:53 -0300

    Me gusta hdf5 :3

--- TPfin.R
+++ TPfin.R
@@ -1,38 +1,53 @@
-#library(hdf5r)
+library(hdf5r)
+library(ggplot2)
 
-info<-H5File$new("2A.GPM.DPR.V9-20211125.20231030-S074316-E091549.054943.V07B.HDF5",mode = "r+",fi)
-
-# Loque quiero: tasa_precip<-info[[2]][[8]][["precipRateESurface"]]$read()
-paste(info,"[['FS']]","[['SLV']]","[['precipRateESurface']]","[['precipRateESurface']]","$read()",quote(F))
-
-info[["FS"]][["SLV"]][["precipRateESurface"]][["precipRateESurface"]]$read()
-
-
-A <- strsplit("FS/SLV/precipRateESurface",split = "/")
-
-b<-eval(parse(text=(A[[1]][1]),keep.source = T))
-
-a<- eval(parse(text = paste0("info[[",A[[1]][1],"]]$read()"),keep.source = T))
-
-paste0("info","[[""",A[[1]][1],"""]]$read()")
-
-#eval(paste0(info,[[mod1]],[[mod2]]))
-
-list("FS/SLV/precipRateESurface")
-
-
-lista_variables<-function(ruta_archivo,lista_ruta){
-  #Formato de las rutas: "FS/SLV/precipRateESurface"
-  archivo<-H5File$new(filename = ruta_archivo,mode = "r+")
-  list_var<-list()
-  for (i in 1:length(lista_ruta)) {
-    variable<-paste(archivo,lista_ruta[[i]],sep="")
-    list[strsplit()]
+lista_variables<-function(ruta_archivo,lista_rutas){
+  #Ingresan rutas con formato "FS/SLV/variable"
+  
+  #Transformo la ruta a formato [["FS"]][["SLV"]][["variable"]]
+  
+  #Obtengo una lista con la informacion de la variable
+  
+  archivo<-H5File$new(ruta_archivo,mode = "r+")
+  lista_var<-list()
+  for (i in 1:length(lista_rutas)) { 
+  #Separo la ruta en funcion de /, y averiguo la cantidad de elementos
+    
+    if(length(strsplit(lista_rutas[[i]],"/")[[1]])==1){
+  #En esta parte averiguo que variables son atributos o variables
+      if(info$attr_exists(lista_rutas[[i]])){
+        variable<-info$attr_open(attr_name = lista_rutas[[i]][1])
+        lista_var[[eval(parse(text='strsplit(lista_rutas[[i]][1],"/")[[1]][1]'))]]<-variable
+  #Genero un elemento de la lista con la informacion de la variable y la nombro como la variable
+      }else{
+        variable<-info[[lista_rutas[[i]]]]
+        lista_var[[lista_rutas[[i]]]]<-variable
+      }
+  #Para generar las variables en este tipo de archivo obligatorimente tienen que tener un 
+  #formato [["FS"]][["SLV"]][["variable"]], esto limita demasiado a la hora de querer transformar el formato
+  #ya que hay que colocar comillas dentro de comillas.
+    }else if(length(strsplit(lista_rutas[[i]],"/")[[1]])==2){
+      variable<-eval(parse(text=paste('archivo[["',gsub("/",'"]][["',lista_rutas[[i]][1]),'"]]$read()',sep = "")))
+  
+  #A la ruta le cambio las barras que separan los terminos por los corchetes que lo separarian con la funcion gsub.
+  #Al pegar los terminos queda "archivo[[\"FS\"]][[\"Longitude\"]]$read()", lo que genera un problema con las \.
+  #parse es una funcion que genera una expresion sin evaluar, la cual me soluciona el problema de las \ y las comillas que se forman
+  #por la funcion paste. Como quiero los datos en la variable, con la funcion eval, evaluo el termino y consigo esos datos.
+  
+      lista_var[[eval(parse(text='strsplit(lista_rutas[[i]][1],"/")[[1]][2]'))]]<-variable
+    
+    }else if(length(strsplit(lista_rutas[[i]],"/")[[1]])==3){
+      variable<-eval(parse(text=paste('archivo[["',gsub("/",'"]][["',lista_rutas[[i]][1]),'"]]$read()',sep = "")))
+      lista_var[[eval(parse(text='strsplit(lista_rutas[[i]][1],"/")[[1]][3]'))]]<-variable
+    }
   }
+  h5close(archivo)
+  return(lista_var)
 }
 
-A <-list()
-A[strsplit("FS/SLV/precipRateESurface",split = "/")[[1]][3]]<-270
+nombre_archivo<-"2A.GPM.DPR.V9-20211125.20231030-S074316-E091549.054943.V07B.HDF5"
+data<-paste(getwd(),nombre_archivo,sep = "/")
+rutas<-list("FileHeader","InputRecord","AlgorithmRuntimeInfo","NavigationRecord","FileInfo","JAXAInfo","FS/Latitude",
+            "FS/Longitude","FS/SLV/precipRateESurface","FS/SLV/zFactorFinalESurface")
 
-strsplit("FS/SLV/precipRateESurface",split = "/")[[1]][3]
-variable<-cat("info[[",paste(A[[1]][1],A[[1]][2],A[[1]][3],sep = "']][['"),"']]$read()",sep = "")
+datos<-lista_variables(data,rutas)
